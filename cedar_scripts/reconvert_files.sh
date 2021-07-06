8e6b329e7e8aed833f027011e78d93dc48631cf9
Joanna-Gao <joannagao6918@gmail.com>
2021-07-06 06:01:13 -0700

    Added two bash scripts for root to npz processing with existing data files

---
+++ cedar_scripts/reconvert_files.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --account=rpp-blairt2k
+#SBATCH --time=3:00:00
+#SBATCH --mem-per-cpu=16G
+#SBATCH --output=%x-%a.out
+#SBATCH --error=%x-%a.err
+#SBATCH --cpus-per-task=1
+
+# Reconverts the files given in the argument
+
+# exit when any command fails
+set -e
+
+ulimit -c 0
+
+source /home/jgao/sourceme.sh
+long_data_dir=${data_dir}/HKHybrid/WCSim
+
+for var in "$@"; do
+  for file in `echo $var`; do
+    # echo "The current file is $file"
+    npzdir="$(dirname "$file")"
+    # echo "The directory containing file is extracted, which is $npzdir"
+    npzdir="${npzdir#${long_data_dir}}"
+    npzdir="/scratch/jgao/data${npzdir}"
+    # echo "The npz dir is $npzdir"
+    mkdir -p "$npzdir"
+    echo "[`date`] converting ${file} to numpy file in ${npzdir}"
+    python "$DATATOOLS/root_utils/event_dump.py" "${file}" -d "${npzdir}"
+  done
+done
+
+endtime="`date`"
+echo "[${endtime}] Completed"
---
+++ cedar_scripts/reconvert_shorttank_jobs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Example script to set up and submit batch jobs
+
+# exit when any command fails
+set -e
+
+# initial environment setup
+export DATATOOLS=/project/rpp-blairt2k/jgao/DataTools
+cd $DATATOOLS/cedar_scripts
+source /home/jgao/sourceme.sh
+
+# name and output data directory for this run
+name=HKHybrid
+data_dir=/project/rpp-blairt2k/machine_learning/data
+
+log_dir="/scratch/jgao/log/${name}"
+mkdir -p $log_dir
+cd $log_dir
+
+# 0-999 root files for mu-, 0-999 root files for e- 
+for i in "" {924..999}; do
+  # f="${data_dir}/${name}/WCSim/*/*/*/*/*_${i}[0-9].root"
+  f="${data_dir}/${name}/WCSim/*/*/*/*/*_${i}.root"
+  sbatch --time=3:0:0 --job-name=rc${f##*_} "${DATATOOLS}/cedar_scripts/reconvert_files.sh" "$f"
+done
