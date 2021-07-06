c20ca8c498d60b46560739cddd028030071153a0
Joanna-Gao <joannagao6918@gmail.com>
2021-07-06 08:23:24 -0700

    Added scripts to convert npz to h5 files on cedar

--- cedar_scripts/make_digihit_h5.sh
+++ cedar_scripts/make_digihit_h5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=rpp-blairt2k
-#SBATCH --time=3-0:0:0
-#SBATCH --mem-per-cpu=8000
+#SBATCH --time=0:01:0
+#SBATCH --mem-per-cpu=1M
 #SBATCH --output=%x-%a.out
 #SBATCH --error=%x-%a.err
 #SBATCH --cpus-per-task=1
@@ -14,7 +14,8 @@
 
 ulimit -c 0
 
-#source /project/rpp-blairt2k/machine_learning/production_software/DataTools/cedar_scripts/sourceme.sh
+#source /project/rpp-blairt2k/machine_learning/production_software/DataTools/\
+# cedar_scripts/sourceme.sh
 source ~/sourceme.sh
 
 virtualenv --no-download $SLURM_TMPDIR/env
@@ -22,7 +23,8 @@ source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index h5py
 
-#cd /project/rpp-blairt2k/machine_learning/production_software/DataTools/root_utils/
+#cd /project/rpp-blairt2k/machine_learning/production_software/DataTools/\
+# root_utils/
 cd /home/jgao/work/DataTools/root_utils/
 
 # initially save to SLURM_TMPDIR for speed
@@ -36,8 +38,10 @@ cd /home/jgao/work/DataTools/root_utils/
 #  fi
 #done
 
-echo "python np_to_digihit_array_hdf5.py $@"
-python np_to_digihit_array_hdf5.py "$@"
+# need to pass in output path + output name with option "-o", and the name of
+# the npz files
+echo "python np_to_digihit_array_hdf5.py -o $1 ${@:2}"
+# python np_to_digihit_array_hdf5.py -o "$1" "${@:2}"
 
 #echo "cp $tmpfile $outfile"
 #cp "$tmpfile" "$outfile"
---
+++ cedar_scripts/run_h5_digi_jobs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Example script to set up and submit batch jobs
+
+# exit when any command fails
+set -e
+
+# initial environment setup
+export DATATOOLS=/project/rpp-blairt2k/jgao/DataTools
+cd $DATATOOLS/cedar_scripts
+
+# project name, input data directory and output file name for this run
+name=HKHybrid
+data_dir=${SLURM_TMPDIR}/data
+ouput_name=${SLURM_TMPDIR}/data/${name}/test_2_file.hdf5
+
+log_dir="/scratch/jgao/log/${name}/h5"
+mkdir -p $log_dir
+cd $log_dir
+
+# 0-999 npz files for mu-, 0-999 npz files for e-
+# Try 2 files of 1 e- 1 mu- at a time, concatinating file names together
+f=
+for i in {0..0}; do
+  f+="${data_dir}/${name}/*/*/*/*/*_${i}.npz "
+done
+
+sbatch --time=0:01:0 --job-name=npzh5h "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" "$output_name" "$f"
+
+# Try 20 files of 10 e- 10 mu- at a time
