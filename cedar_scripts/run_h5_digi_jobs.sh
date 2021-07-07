c959a869e7aef69c473350f0d119e3e66f735067
Joanna-Gao <joannagao6918@gmail.com>
2021-07-07 15:45:09 -0700

    Updating the h5 bash script

--- cedar_scripts/run_h5_digi_jobs.sh
+++ cedar_scripts/run_h5_digi_jobs.sh
@@ -11,7 +11,8 @@ cd $DATATOOLS/cedar_scripts
 # project name, input data directory and output file name for this run
 name=HKHybrid
 data_dir=/scratch/jgao/data
-output_name="/scratch/jgao/data/test_2_file.hdf5"
+output_dir=/scratch/jgao/data/h5
+mkdir -p $output_dir
 
 log_dir="/scratch/jgao/log/h5"
 mkdir -p $log_dir
@@ -20,8 +21,12 @@ cd $log_dir
 # 0-999 npz files for mu-, 0-999 npz files for e-
 # Try 2 files of 1 e- 1 mu- at a time, concatinating file names together
 for i in {0..0}; do
-  f="${data_dir}/${name}/*/*/*/*/*_${i}.npz "
-  sbatch --time=2:0:0 --job-name=npz2h5 "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" "$output_name" "$f"
+  for j in e- mu-; do
+    f="${data_dir}/${name}/${j}/*/*/*/*_${i}.npz "
+    sbatch --time=2:0:0 --job-name=npz2h5 \
+      "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" \
+      "${output_dir}/${j}_${i}.hdf5" "$f"
+  done
 done
 
 # Try 20 files of 10 e- 10 mu- at a time
