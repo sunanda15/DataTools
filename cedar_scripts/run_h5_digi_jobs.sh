779aef7c37c7d68ab4cf4cb53704e0eb24a3642e
Joanna-Gao <joannagao6918@gmail.com>
2021-07-09 02:21:51 -0700

    Reprocessing mu- 600-799 and 900-999

--- cedar_scripts/run_h5_digi_jobs.sh
+++ cedar_scripts/run_h5_digi_jobs.sh
@@ -20,23 +20,12 @@ cd $log_dir
 
 # 0-999 npz files for mu-, 0-999 npz files for e-
 # 0-9 in 1 file, 10-99 in 1 file, 100-199 ... 900-999.
-# Turns out the merge_h5.py file needs modifying, I'm too lazy, processing
-# all npz files together for now
-# for i in {1..9}; do
-#   for j in e- mu-; do
-#     f="${data_dir}/${name}/${j}/*/*/*/*_${i}[0-9][0-9].npz "
-#     sbatch --time=2:0:0 --job-name=npz2h5_${j}_${i} \
-#       "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" \
-#       "${output_dir}/${j}_${i}.hdf5" "$f"
-#   done
-# done
+for i in {6..7} "9"; do
+  for j in mu-; do
+    f="${data_dir}/${name}/${j}/*/*/*/*_${i}[0-9][0-9].npz "
+    sbatch --time=4:0:0 --job-name=npz2h5_${j}_${i} \
+      "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" \
+      "${output_dir}/${j}_${i}.hdf5" "$f"
+  done
+done
 
-f="${data_dir}/${name}/mu-/*/*/*/*_5[0-9][0-9].npz"
-sbatch --time=4:0:0 --job-name=npz2h5_mu-_5 \
-  "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" \
-  "${output_dir}/mu-_5.hdf5" "$f"
-
-f1="${data_dir}/${name}/e-/*/*/*/*_9[0-9][0-9].npz"
-sbatch --time=4:0:0 --job-name=npz2h5_e-_9 \
-  "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" \
-  "${output_dir}/e-_9.hdf5" "$f1"
